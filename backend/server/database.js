f76b99092ec9c4159670165ef8963bdc8bf88305
Mauro Vargas <vmauricio823@gmail.com>
2019-07-16 13:17:15 -0500

    ..

--- backend/server/database.js
+++ backend/server/database.js
@@ -1,7 +1,7 @@
 //Neode es un OGM (Object Graph Mapper) para node.js
 // Aquí se realiza la conexión a la base de datos
 const Neode = require('neode');
-
+//
 // Exportar la instancia de la sesion de neo4j para usarla en otros lados (controladores)
 module.exports =  
     new Neode('bolt://localhost:7687', 'neo4j', '123')
