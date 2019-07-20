034d6e9b7f5d9caa8a108d271df95cc2c62bbfb0
Mauro Vargas <vmauricio823@gmail.com>
2019-07-20 18:03:43 -0500

    url api

--- public/main-es2015.js
+++ public/main-es2015.js
@@ -926,7 +926,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    URL_API: 'http://localhost:3000'
+    URL_API: ''
 };
 /*
  * For easier debugging in development mode, you can import the following file
--- public/main-es5.js
+++ public/main-es5.js
@@ -957,7 +957,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 var environment = {
     production: false,
-    URL_API: 'http://localhost:3000'
+    URL_API: ''
 };
 /*
  * For easier debugging in development mode, you can import the following file
